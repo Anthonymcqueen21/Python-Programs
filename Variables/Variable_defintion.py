135981303d0bad734d2b5480b54b8873d1aed870
Anthony Perez <stockcart21@yahoo.com>
2017-03-23 20:02:26 -0700

    Create Variable_defintion.py
    Variables are used to store values so I can refer to them later. A variable is like a label, and use '=' this is an assignment operator.

---
+++ Variables/Variable_defintion.py
@@ -0,0 +1,8 @@
+a = b = 2
+print("a = " + str(a))
+print("b = " + str(b))
+
+greetings = "greetings"
+print("greetings = " + str(greetings))
+greetings = "Python Programmer"
+print("greetings = " + str(greetings))
