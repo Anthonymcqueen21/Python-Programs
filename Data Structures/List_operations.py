44e1d050afc16095b348602b6a864d1699db030e
Anthony Perez <stockcart21@yahoo.com>
2017-04-07 23:07:52 -0700

    Create List_operations.py
    Replace 'dino' with 'dinosaur' in the animals list.

---
+++ Data Structures/List_operations.py
@@ -0,0 +1,10 @@
+animals = ['elephant', 'lion', 'tiger', 'giraffe'] #create new list
+print(animals)
+
+animals += ["monkey", "dog"]  #adding two items to the list
+print(animals)
+
+animals.append("dino") #add one more item to the list using append() method
+
+[replace 'dino' with 'dinosaur']
+print(animals)
