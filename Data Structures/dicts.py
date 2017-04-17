a4a608fee8db38a5944d7f750bebc96e8d20999c
Anthony Perez <stockcart21@yahoo.com>
2017-04-16 19:55:16 -0700

    Create dicts.py
    A dictionary is similar to a list, except that you access its values by looking up a key instead of an index. A key can be any string or a number. Dictionaries are enclosed in curly braces e.g. dct = {'key1' : "value1", 'key2' : "value2"}. 
    
    
    
    Print Jane's phone number from phone_book.

---
+++ Data Structures/dicts.py
@@ -0,0 +1,11 @@
+# create new dictionary.
+phone_book = {"John": 123, "Jane": 234, "Jerard": 345}
+
+# Add new item to the dictionary
+phone_book["Jill"] = 345
+print(phone_book)
+
+# Remove key-value pair from phone_book
+del phone_book['John']
+
+print(phone_book('234')
