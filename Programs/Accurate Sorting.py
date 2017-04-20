465fa087d2b741594c62dd5747d3db1026e323a4
Anthony Perez <stockcart21@yahoo.com>
2017-04-19 17:40:12 -0700

    Create Accurate Sorting.py

---
+++ Programs/Accurate Sorting.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import sys
+
+def isSortable(list):
+    for i in range(0, len(list)-1):
+        e = list[i]
+        next_e = list[i+1]
+        if e <= next_e:
+            continue
+        elif e-next_e==1:
+            list[i], list[i+1] = list[i+1], list[i]
+        else:
+            return "No"
+    return "Yes"
