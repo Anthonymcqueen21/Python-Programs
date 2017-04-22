866ec58ab11565cb5c0c8889a6686b79402a6061
Anthony Perez <stockcart21@yahoo.com>
2017-04-21 22:34:07 -0700

    Create Simple Calculator.py
    This is a simple calculator

---
+++ Python Project/Simple Calculator.py
@@ -0,0 +1,22 @@
+while True:
+print("Options:")
+print("Enter add to add two numbers")
+print("Enter subtract to subtract two numbers")
+print("Enter multiply to multiply two numbers")
+print("Enter divide to divide two numbers")
+print("Enter 'quit' to end the program")
+user_input = input(":")
+
+if user_input == "quit":
+   break
+elif user_input == "add":
+   break:
+elif user_input == "subtract":
+  break:
+elif user_input == "multiply":
+  break:
+elif user_input == "divide":
+
+else:
+   print("Unknown input")
+
