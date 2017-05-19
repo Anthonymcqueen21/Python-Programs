0cd2cef347335c3a5b5210d01b5160a3aba6995a
Anthony Perez <stockcart21@yahoo.com>
2017-05-18 19:44:23 -0700

    Create Maintenance issues.py
    Total of monthly cost

---
+++ Programs/Maintenance issues.py
@@ -0,0 +1,17 @@
+# Maintenance for rich people
+# Demonstrates the amount spend in a day !!!
+
+print("Enlarged trust")
+
+car = input("Ferrari Enzo")
+rent = input("San Francisco Apartment.")
+aircraft = input("F-14 Tomcat.")
+bribes = input("Bought off politicians.")
+quizene = input("Eating out.")
+staff = input("Solid Snake.")
+guru = input("Python Master.")
+games = input("Medal Gear Franchise.")
+
+total = car + rent + aircraft + bribes + quizene + staff + guru + games
+
+print("\nGrand Total:", total)
