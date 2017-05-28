f833a61aece12a26b37e72f4cb660d9c9197c372
Anthony Perez <stockcart21@yahoo.com>
2017-05-28 11:38:06 -0700

    Create Creating Aliens.py
    Creating an alien dictionary within a list using the elif statement.

---
+++ Programs/Creating Aliens.py
@@ -0,0 +1,23 @@
+#Make an empty list for storing aliens.
+aliens = []
+
+# Make about 30 aliens for battle
+for alien_number in range (0, 30):
+    new alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+    
+for alien in aliens[0:3]:
+    if alien['color'] == 'green'
+        alien['color'] == 'yellow'
+        alien['speed'] == 'medium'
+        alien['points'] == 10
+    elif alien['color'] = 'yellow'
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+              
+# Show the first 5 aliens.
+for alien in aliens[0:5]
+    print(alien)
+print("...")
+
