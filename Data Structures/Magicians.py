bae294f94b2d914f43aefc7684abc89c8cf17c70
Anthony Perez <stockcart21@yahoo.com>
2017-06-09 23:44:55 -0700

    Create Magicians.py

--- Data Structures/Magicians.py
+++ Data Structures/Magicians.py
@@ -1,5 +1,5 @@
 magicians['Jared', 'Jax', 'Carter', 'Phillip']
 for magician in magicians:
-    print(magician.title() + ". that was great tricks !")
-    print("I cant wait to see more tricks, " + magician.title() + " \n")
+print(magician.title() + ". that was great tricks !")
+print("I cant wait to see more tricks, " + magician.title() + " \n")
 print("Thank you, everyone. That was a magic great magic show")
