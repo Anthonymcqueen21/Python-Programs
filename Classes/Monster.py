ceddc29e3fbae60d412aebcf37c0b68ba0cd2d81
Anthony Perez <stockcart21@yahoo.com>
2017-06-22 17:53:49 -0700

    Create Monster.py
    Creating a simple class using python.

---
+++ Classes/Monster.py
@@ -0,0 +1,15 @@
+class Monster():
+    """A simple attempt to model a monster."""
+    
+    def __init__(self, name. monster):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+        
+    def kill(self):
+        """Simulate a monster killing a bunch of people."""
+        print(self.name.title() + " is now killing.")
+        
+    def destroy(self):
+        """Simulate destroying a major city."""
+        print(self.name.title() + " destroy the city!")
