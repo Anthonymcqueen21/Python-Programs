a49acafb194228e2e4093e11b720563812433215
Anthony Perez <stockcart21@yahoo.com>
2017-06-29 11:36:43 -0700

    Create Creating a monster.py
    Another class with instances.

---
+++ Classes/Creating a monster.py
@@ -0,0 +1,17 @@
+class Monster():
+    """Creating a monster to represent the king of the monsters."""
+    
+    def __init__(self, name, species):
+        """Initialize all atrributes of the monster."""
+        self.monster = monster
+        self.species = species
+        self.abilites = abilites
+        
+    def get_descriptive_name(self):
+        """Return a formatted name of monster."""
+        long_name = str(self.year) + ' '  + self.name + ' ' + self.monster
+        return long_name.title()
+        
+    my_new_monster = Monster('Godzilla', 2014)
+    print(my_new_monster.get_descriptive_name())
+    
