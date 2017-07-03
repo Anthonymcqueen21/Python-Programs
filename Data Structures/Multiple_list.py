b11f779e1ac49b9e1888980118b66802cd9e3a85
Anthony Perez <stockcart21@yahoo.com>
2017-07-03 14:01:30 -0700

    Update Multiple_list.py

--- Data Structures/Multiple_list.py
+++ Data Structures/Multiple_list.py
@@ -1,12 +1,9 @@
 availible_cars = ['Ford Mustang', 'Subaru WRX sti', 'Mitsubishi Evo',
                   'Nissan 240']
-
 requested_cars = 'Subaru WRX sti', 'Ford Mustang'']
-
 for requested_cars in requested_cars:
     if requested_car in availible_cars:
         print("Adding " + requested_car + ".")
     else:
-        print("Sorry we dont have " + requested_car + ".")
-        
-print("\nFinished making the right car purchase")
+      print("Sorry we dont have " + requested_car + ".")
+      print("\nFinished making the right car purchase")
