c8d84fc74cc4b09d287d344f6ad76ea3bb6221f4
Anthony Perez <stockcart21@yahoo.com>
2017-07-08 12:19:25 -0700

    Create Exceptions to prevent crashes.py

---
+++ Files - Exceptions/Exceptions to prevent crashes.py
@@ -0,0 +1,22 @@
+try:
+    divide(5/0)
+except ZeroDivisonOperator:
+    print("You cant divide by zero.")
+    
+print("Give me two numbers, and i wil divide them.")
+print("Enter 'q' to quit.")
+
+while True:
+    first_number = input("\nFirst number:")
+    if first_number == 'q':
+        break
+    second_number = input("\nSecond number:"
+    try:
+                       
+    if second_number = 'q':
+        break
+    answer = int(first_number) / int(second_number)
+    except ZeroDivisionOperator:
+        print("You cant divide by 0!")
+    else:
+        print(answer)
