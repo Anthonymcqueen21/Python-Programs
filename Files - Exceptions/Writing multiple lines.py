4b8b9a8d5247031c9c0d5209ce15aa4246d9e682
Anthony Perez <stockcart21@yahoo.com>
2017-07-08 07:48:00 -0700

    Create Writing multiple lines.py
    Here is a file where you can write multiple lines

---
+++ Files - Exceptions/Writing multiple lines.py
@@ -0,0 +1,5 @@
+filename = 'competitive programming.txt'
+
+with open(filename, 'w') as file_object:
+    file_object.write("I love code competition.")
+    file_object.write("I want to use Python to win Top coder, Google code Jam, Codechef smackdown.")
