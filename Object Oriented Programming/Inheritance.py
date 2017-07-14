bb7c8db649b37cd70d8334ccc7d4c36d12ee7d82
Anthony Perez <stockcart21@yahoo.com>
2017-07-13 17:19:45 -0700

    Update Inheritance.py

--- Object Oriented Programming/Inheritance.py
+++ Object Oriented Programming/Inheritance.py
@@ -1,15 +1,12 @@
 class A:
   def method(self):
     print("A method")
-    
 class B(A):
   def another_method(self):
     print("B method")
-    
 class C(B):
   def third_method(self):
     print("C method")
-    
 c = C()
 c.method()
 c.another_method()
