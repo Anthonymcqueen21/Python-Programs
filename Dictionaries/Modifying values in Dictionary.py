cf26999d1c9ae44ac4d1ceb87f6b03e15982b8ee
Anthony Perez <stockcart21@yahoo.com>
2017-07-18 07:27:01 -0700

    Update Modifying values in Dictionary.py

--- Dictionaries/Modifying values in Dictionary.py
+++ Dictionaries/Modifying values in Dictionary.py
@@ -1,6 +1,5 @@
 alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
 print("original x-position: " + str(alien_0['x_position']))
-
 #Moving the alien to the right.
 #Determining how far to move the alien based on its current speed.
 if alien_0['speed'] == 'slow':
@@ -12,6 +11,5 @@ else:
     x_increment = 3
 # The new position is the old position plus the increment.
 alien_0['x_position'] = alien_0['x_position'] + x_increment
-
 print("New x-position: " + str(alien_0['x_position']))
 
