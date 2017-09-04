36763a73c882722166d0b4559fd7b142875f7ae1
Anthony Perez <stockcart21@yahoo.com>
2017-09-03 22:35:15 -0700

    Update List_operations.py

--- Data Structures/List_operations.py
+++ Data Structures/List_operations.py
@@ -1,11 +1,8 @@
 animals = ['elephant', 'lion', 'tiger', 'giraffe'] #create new list
  print(animals)
-
 animals += ["monkey", "dog"]  #adding two items to the list
  print(animals)
-
 animals.append("dinosaur") #add one more item to the list using append() method
  print(animals)
-
 animals += ['dinosaur', 'dinosaur']
  print(animals)
