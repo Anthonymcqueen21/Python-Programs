b5522c2c8c0277cf22124d402380dec73d795c48
Anthony Perez <stockcart21@yahoo.com>
2017-09-15 12:52:47 -0700

    Update Define name.py

--- Functional Programming/Define name.py
+++ Functional Programming/Define name.py
@@ -3,6 +3,7 @@ def get_formatted_name(first_name, last_name):
     full_name = first_name + ' '  + last_name
     return full_name.title()
 
+
 # This is an Infinite loop !
 while True:
     print("\nPlease tell me your:")
